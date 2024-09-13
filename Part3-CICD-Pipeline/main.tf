58c92b1ab6a79c8cd3a2d7ff42279980d314c511
ostafon1 <154944433+ostafon1@users.noreply.github.com>
2024-09-12 19:30:34 -0500

    Create main.tf

---
+++ Part3-CICD-Pipeline/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "WebServer"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              EOF
+}
+
+output "instance_ip" {
+  value = aws_instance.web.public_ip
+}
